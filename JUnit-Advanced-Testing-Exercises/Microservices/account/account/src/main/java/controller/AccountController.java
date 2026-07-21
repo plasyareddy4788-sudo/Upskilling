fd34f9a7d2aeb1b38ca2b269ef96ccae31c88f15
plasyareddy4788-sudo <plasyareddy4788@gmail.com>
2026-07-21 21:10:14 +0530

    Add files via upload

---
+++ JUnit-Advanced-Testing-Exercises/Microservices/account/account/src/main/java/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.cognizant.account.controller;
+
+import org.springframework.web.bind.annotation.*;
+import com.cognizant.account.model.Account;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @GetMapping("/{number}")
+    public Account getAccount(@PathVariable String number) {
+        return new Account(number, "Savings", 234343);
+    }
+}
